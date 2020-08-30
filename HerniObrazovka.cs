fdc9d14e0e685657b32b5ed4f7632270222279b5
Vojtěch Bukovský <vojbukovsky@gmail.com>
2020-08-30 12:27:04 +0200

    Přidejte soubory projektu.

---
+++ HerniObrazovka.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Pexeso
+{
+    class HerniObrazovka
+    {
+        private int Rozmer;
+        //Je deklarovaná informacemi z hlavního programu
+        private int OdhaleneKarty = 0, PocitadloTahu = 0;
+        private char[,] HerniPole;
+        private StavKarty[,] PoleStavu;
+        //enumy mohou být pole!
+        private char RubKarty;
+        private Pozice OtocenaKarta;
+        private char LicKarty = 'A';
+        public HerniObrazovka(Int16 _rozmer=4, char _rubkarty='#')
+        {
+            RubKarty = _rubkarty;
+            Rozmer = _rozmer;
+            HerniPole = new char[Rozmer, Rozmer];
+            PoleStavu = new StavKarty[Rozmer, Rozmer];
+            RozdejKarty();
+        }
+        private void RozdejKarty()
+        {
+            ArrayList volnePozice = new ArrayList();
+            for (int y = 0; y < Rozmer; y++)
+            {
+                for (int x = 0; x < Rozmer; x++)
+                {
+                    Pozice pozice = new Pozice(x, y);
+                    //když vytvářím pozici, tak se spustí konstruktor, ve kterém se jí přiřadí tato čísla - x a y dle loopu
+                    volnePozice.Add(pozice);
+                    PoleStavu[x, y] = StavKarty.Skryta;
+                }
+            }
+            while (volnePozice.Count >= 2)
+            {
+                Random generatorCisel = new Random();
+                int cislo = generatorCisel.Next(volnePozice.Count);
+                Pozice prvniKarta = (Pozice)volnePozice[cislo];
+                volnePozice.Remove(prvniKarta);
+                //dokud zbývají nepřiřazené dvojice, tak se vygeneruje náhodné číslo, které náhodně vybere jednu kartu. Stejně tak s druhou kartou.
+                cislo = generatorCisel.Next(volnePozice.Count);
+                Pozice druhaKarta = (Pozice)volnePozice[cislo];
+                volnePozice.Remove(druhaKarta);
+
+                HerniPole[prvniKarta.X, prvniKarta.Y] = LicKarty;
+                HerniPole[druhaKarta.X, druhaKarta.Y] = LicKarty;
+                //
+                LicKarty++;
+                //toto změní "A" na "B" atd. Prostě se vymění znak za jinou dvojici znaků.
+            }
+        }
+        public void VykresliPlochu()
+        {
+            Console.Clear();
+
+            for (int y = 0; y < Rozmer; y++)
+            {
+                for (int x = 0; x < Rozmer; x++)
+                {
+                    switch(PoleStavu[x,y])
+                    {
+                        case StavKarty.Skryta:
+                            Console.Write(RubKarty);
+                            break;
+                        case StavKarty.Otocena:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(HerniPole[x, y]);
+                            Console.ResetColor();
+                            break;
+                        case StavKarty.Odstranena:
+                            Console.Write(" ");
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Tahy: " + PocitadloTahu);
+            Console.ResetColor();
+            //vykreslí se postupně všechny prvky herního pole
+        }
+        public bool VseOdhaleno()
+        {
+            return OdhaleneKarty == Rozmer * Rozmer;
+            //jednoduchá funkce, která kontroluje, jestli byly všechny karty odhaleny
+        }
+        public bool ValidniKarta(int x, int y)
+        {
+            return (x >= 0 && x < Rozmer) && (y >= 0 && y < Rozmer) && (PoleStavu[x, y] == StavKarty.Skryta);
+            //vrátí se true pokud - karta nemá větší rozměry x a y než daný rozměr a když je karta skrytá
+        }
+        public void OtocPrvniKartu(int x, int y)
+        {
+            //Spustí když správně zadáme souřadnice pro otočení první karty.
+            OtocenaKarta = new Pozice(x, y);
+            PoleStavu[x, y] = StavKarty.Otocena;
+            PocitadloTahu++;
+            VykresliPlochu();
+        }
+        public void OtocDruhouKartu(int x, int y)
+        {
+            //Spustí když správně zadáme souřadnice pro otočení druhé karty.
+            PoleStavu[x, y] = StavKarty.Otocena;
+            PocitadloTahu++;
+            VykresliPlochu();
+            Console.ReadKey(true);
+            if (HerniPole[OtocenaKarta.X, OtocenaKarta.Y] == HerniPole[x, y])
+            {
+                //kontrola, jestli jsou obě karty stejné a jejich následné odstranění
+                PoleStavu[OtocenaKarta.X, OtocenaKarta.Y] = StavKarty.Odstranena;
+                PoleStavu[x, y] = StavKarty.Odstranena;
+                OdhaleneKarty += 2;
+            }
+            else
+            {
+                PoleStavu[OtocenaKarta.X, OtocenaKarta.Y] = StavKarty.Skryta;
+                PoleStavu[x, y] = StavKarty.Skryta;
+            }
+        }
+        enum StavKarty
+        {
+            Skryta, Otocena, Odstranena
+        }
+        struct Pozice
+        {
+            private readonly int x;
+            public int X
+            {
+                get
+                {
+                    return x;
+                }
+            }
+            private readonly int y;
+            public int Y
+            {
+                get
+                {
+                    return y;
+                }
+            }
+            public Pozice(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            // jak to funguje - v loopu se vytvoří struct - ten zavolá konstruktor, který přiřadí k lokálním proměnnám x a y hodnoty x a y z loopu (pomocí this). To znamená, že x a y mohou být změněné POUZE v konstruktoru - kromě toho struktura navrací pouze get, nikoliv set.
+            // Podobně jako se třídami, každý strukt uchovává jiné hodnoty podle jednoho vzoru
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pexeso
+{
+    class Program
+    {
+        static HerniObrazovka herniObrazovka;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Vítejte v Pexesu!");
+            Console.WriteLine("Stiskněte \"a\" pro standartní hru");
+            Console.WriteLine("Stiskněte \"b\" pro vlastní hru");
+            Console.WriteLine("Stiskněte cokoliv jiného pro ukončení");
+            Console.ResetColor();
+            ConsoleKeyInfo Klavesa = Console.ReadKey(true);
+            switch(Klavesa.Key)
+            {
+                case ConsoleKey.A:
+                    herniObrazovka = new HerniObrazovka();
+                    break;
+                case ConsoleKey.B:
+                    Int16 zadej = 1;
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Zadejte prosím velikost herní plochy: ");
+                        try
+                        {
+                            zadej = Convert.ToInt16(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Zadejte prosím číslo");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        finally
+                        {
+                            if (zadej > 10 || zadej < 2 || zadej % 2 != 0)
+                            {
+                                zadej = 1;
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Pozor, číslo nesmí být větší než 10, menší než 2 a musí být sudé!");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                        }
+
+                    }
+                    while (zadej % 2 != 0);
+                    char rub = '#';
+                    bool ex = true;
+                    do
+                    {
+                        Console.WriteLine("Zadejte prosím znak pro rub");
+                        try
+                        {
+                            rub = Convert.ToChar(Console.ReadLine());
+                            ex = false;
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Toto není validní znak!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    } while (ex);
+                    Console.ResetColor();
+                    herniObrazovka = new HerniObrazovka(zadej, rub);
+                    break;
+                default:
+                    return;
+            }
+            while(!herniObrazovka.VseOdhaleno())
+            {
+                herniObrazovka.VykresliPlochu();
+                int x = -1, y = -1;
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Zadejte souřadnice první hrací karty, každou na jeden řádek");
+                    try
+                    {
+                        x = int.Parse(Console.ReadLine());
+                        y = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Prosím zadávejte jen čísla!");
+                        Console.ResetColor();
+                    }
+                    if (!herniObrazovka.ValidniKarta(x, y))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Chybně zadané souřadnice. Prosím zvolte neodkrytou kartu.");
+                        Console.ResetColor();
+                    }
+                }
+                while (!herniObrazovka.ValidniKarta(x,y));
+                Console.ResetColor();
+                herniObrazovka.VseOdhaleno();
+                herniObrazovka.OtocPrvniKartu(x, y);
+                x = -1;
+                y = -1;
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Zadejte souřadnice druhé hrací karty, každou na jeden řádek");
+                    try
+                    {
+                        x = int.Parse(Console.ReadLine());
+                        y = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Prosím zadávejte jen čísla!");
+                        Console.ResetColor();
+                    }
+                    if (!herniObrazovka.ValidniKarta(x, y))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Chybně zadané souřadnice. Prosím zvolte neodkrytou kartu.");
+                        Console.ResetColor();
+                    }
+                }
+                while (!herniObrazovka.ValidniKarta(x, y));
+                Console.ResetColor();
+                herniObrazovka.OtocDruhouKartu(x,y);
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Gratuluji, vyhrál jsi!");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
